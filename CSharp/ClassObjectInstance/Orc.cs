d50d62037754be2b02c494a5c2644b1b93054bce
500errocode <124433102+500errocode@users.noreply.github.com>
2023-02-11 18:52:39 +0900

    C# bassic
    - 함수(정의, 선언)
    - 변수(선언)
    -클래스 사용
    -멤버(멤버변수, 멤버함수)
    -소멸자: 해당 객체가 할당된 영역의 메모리를 해제할 때 호출하는 함수
    -접근 제한자(public, protected, internal, private)

---
+++ CSharp/ClassObjectInstance/Orc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectInstance
+{
+    internal class Orc
+    {
+        public string Name;
+        public int Age;
+        public float cm;
+        public float kg;
+        public string gender;
+
+        public void SayMyName()
+        {
+            Console.WriteLine($"\n이름: {Name}");
+        }
+        public void SayMyInfo()
+        {
+            Console.WriteLine($"나이: {Age}");
+            Console.WriteLine($"키: {cm}");
+            Console.WriteLine($"몸무게: {kg}");
+            Console.WriteLine($"성별: {gender}\n");
+        }
+    }
+}
