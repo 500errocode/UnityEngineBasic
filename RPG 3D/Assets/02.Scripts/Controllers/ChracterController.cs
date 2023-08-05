ec80803bb3d57a3607b6e39950d1887eb2d53395
ehddud0501 <124433102+500errocode@users.noreply.github.com>
2023-08-05 18:48:26 +0900

    RPG3D
    Implemented Jump, Fall, Somersault, Land, Attack behaviour

--- RPG 3D/Assets/02.Scripts/Controllers/ChracterController.cs
+++ RPG 3D/Assets/02.Scripts/Controllers/ChracterController.cs
@@ -6,11 +6,11 @@ using UnityEngine;
 public class ChracterController : MonoBehaviour
 {
     private Animator _animator;
-
-
+    private BehaviourManager _behaviourManager;
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _behaviourManager = GetComponent<BehaviourManager>();
     }
 
     private void Update()
@@ -18,5 +18,30 @@ public class ChracterController : MonoBehaviour
         float gain = Input.GetKey(KeyCode.LeftShift) ? 1.0f : 0.5f;
         _animator.SetFloat("horizontal", Input.GetAxis("Horizontal") * gain);
         _animator.SetFloat("vertical", Input.GetAxis("Vertical") * gain);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_behaviourManager.hasAttacked)
+                _behaviourManager.ChangeStateForcely(StateID.Attack);
+            _behaviourManager.ChangeState(StateID.Attack);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_behaviourManager.isGrounded)
+            {
+                if (_behaviourManager.hasJumped == false)
+                {
+                    _behaviourManager.ChangeState(StateID.Jump);
+                }
+            }
+            else
+            {
+                if (_behaviourManager.hasSomersaulted == false)
+                {
+                    _behaviourManager.ChangeState(StateID.Somersault);
+                }
+            }
+        }
     }
 }
